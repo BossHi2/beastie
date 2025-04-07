4b293a6356573cdd306c9443eb7aab5fb3e3cdde
BossHi2 <157933871+BossHi2@users.noreply.github.com>
2025-04-06 21:28:50 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+const beast = document.getElementsByClassName("beast")[0];
+var prevNum = 0;
+
+const cursor = document.getElementsByClassName("cursor")[0];
+
+cursor.click(function(e){e.preventDefault();});
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    cursor.animate({
+        left: `${clientX-200}px`,
+        top: `${clientY-100}px`
+    
+    }, {duration: 1, fill: "forwards"})
+
+}
+
+function beastClick(){
+    var randNum = 1+(Math.random() * 79);
+    while(Math.abs(randNum-prevNum) < 3)
+        randNum = 1+(Math.random() * 79);
+    var pos = randNum + 'vw';
+    beast.style.left = pos;
+    prevNum = randNum;
+}
+
+beast.addEventListener('click', beastClick);
