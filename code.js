b589e6bacce792be32e24781235f559e121f064c
BossHi2 <157933871+BossHi2@users.noreply.github.com>
2025-04-08 16:39:29 -0500

    final push hopefully

--- code.js
+++ code.js
@@ -1,21 +1,6 @@
 const beast = document.getElementsByClassName("beast")[0];
 var prevNum = 0;
 
-const cursor = document.getElementsByClassName("cursor")[0];
-
-cursor.click(function(e){e.preventDefault();});
-
-document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
-
-    cursor.animate({
-        left: `${clientX-200}px`,
-        top: `${clientY-100}px`
-    
-    }, {duration: 1, fill: "forwards"})
-
-}
-
 function beastClick(){
     var audio = new Audio('bark.mp3');
     audio.play();
@@ -23,7 +8,10 @@ function beastClick(){
     while(Math.abs(randNum-prevNum) < 3)
         randNum = 1+(Math.random() * 79);
     var pos = randNum + 'vw';
-    beast.style.left = pos;
+    beast.animate({        
+        left:pos,
+    }, {duration:200, fill: "forwards"})
+
     prevNum = randNum;
 }
 
